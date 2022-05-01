282aeca4eba4e9c1a3a595aee7bfcaf85da41d9d
slevine3 <levinesam3@gmail.com>
2022-05-01 15:56:53 +0300

    begin styled\ components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+height: 60px;
+
+`;
+const Wrapper = styled.div`
+padding: 10px 20px; 
+`;
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Wrapper>Navbar</Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
