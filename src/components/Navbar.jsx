371e124ed7e39d2d59914744f6580496a9e7d52e
slevine3 <levinesam3@gmail.com>
2022-05-02 13:21:17 +0300

    packages added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,42 @@
 import React from "react";
 import styled from "styled-components";
+import { Search } from "@mui/icons-material";
 
 const Container = styled.div`
-height: 60px;
-
+  height: 60px;
 `;
 const Wrapper = styled.div`
-padding: 10px 20px; 
+  display: flex;
+  justify-content: space-between;
+  padding: 10px 20px;
 `;
 
+const Left = styled.div`
+  flex: 1;
+`;
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+
+const Center = styled.div`
+  flex: 1;
+`;
+const Right = styled.div`
+  flex: 1;
+`;
 const Navbar = () => {
   return (
     <Container>
-      <Wrapper>Navbar</Wrapper>
+      <Wrapper>
+        <Left><Language>EN</Language></Left>
+        <searchContainer>
+          input 
+       <Search />
+        </searchContainer>
+        <Center>Center</Center>
+        <Right>Right</Right>
+      </Wrapper>
     </Container>
   );
 };
