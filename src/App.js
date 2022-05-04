6f1f2834bd3dbc86ac92b316cf8f8c0b11ceb357
slevine3 <levinesam3@gmail.com>
2022-05-04 17:51:51 +0300

    login and register completed. Cart started

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css'
+import Cart from './pages/Cart';
 
 import Home from './pages/Home';
+import Login from './pages/Login';
 import Product from './pages/Product';
 import ProductList from './pages/ProductList';
+import Register from './pages/Register';
 const App = () =>{
   return (
-<Product />
+<Cart />
   );
 }
 
