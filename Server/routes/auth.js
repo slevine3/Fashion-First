c1fdcfee06d7a2e01c886fe92b305e1c8392bec2
slevine3 <levinesam3@gmail.com>
2022-05-08 16:33:35 +0300

    jwt authentication

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
-const { restart } = require("nodemon");
+const jwt = require("jsonwebtoken");
 //REGISTER
 
 router.post("/register", async (req, res) => {
@@ -34,9 +34,18 @@ router.post("/login", async (req, res) => {
     if (originalPassword !== req.body.password)
       res.status(401).json("Incorrect Password!");
 
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      { expiresIn: "3d" }
+    );
+
     const { password, ...others } = user._doc;
 
-    res.status(200).json(others);
+    res.status(200).json({...others, accessToken});
     res.status(201).json();
   } catch (error) {
     res.status(500).json(error);
--- Server/routes/user.js
+++ Server/routes/user.js
@@ -1,4 +1,74 @@
 const router = require("express").Router();
+const User = require("../models/User");
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
 
+//UPDATE
+
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    );
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//DELETE
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted...");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//GET USER
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+
+    res.status(200).json(others);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//GET ALL USER
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  const query = req.query.new;
+  try {
+    const users = query
+      ? await User.find().sort({ _id: -1 }).limit(5)
+      : await User.find();
+
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+
+//GET USER STATS
+router.get('/stats', verifyTokenAndAdmin, async (req, res)=> {
+    
+})
 
 module.exports = router;
