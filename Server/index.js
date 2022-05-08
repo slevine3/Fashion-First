77c644d63d035ef4c0ca57c289b88e3810ccc2db
slevine3 <levinesam3@gmail.com>
2022-05-08 21:54:47 +0300

    advanced functions for crud operations completed

--- Server/index.js
+++ Server/index.js
@@ -7,11 +7,15 @@ dotenv.config();
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
 
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
+app.use("/api/cart", cartRoute);
+app.use("/api/order", orderRoute);
 
 mongoose
   .connect(process.env.MONGO_URL)
--- Server/routes/order.js
+++ Server/routes/order.js
@@ -9,15 +9,16 @@ const {
 
 //CREATE
 router.post("/", verifyToken, async (req, res) => {
-  const newOrder = new Product(req.body);
+  const newOrder = new Order(req.body);
 
   try {
     const savedOrder = await newOrder.save();
     res.status(200).json(savedOrder);
-  } catch (error) {
-    res.status(500).json(error);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
+
 
 //UPDATE
 
@@ -67,5 +68,33 @@ router.get("/", verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
+// GET MONTHLY INCOME
+
+router.get("/income", verifyTokenAndAdmin, async (req, res) => {
+  const date = new Date();
+  const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
+  const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
+
+  try {
+    const income = await Order.aggregate([
+      { $match: { createdAt: { $gte: previousMonth } } },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+          sales: "$amount",
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: "$sales" },
+        },
+      },
+    ]);
+    res.status(200).json(income);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
