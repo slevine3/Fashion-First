052ece9a8b5b5bf6b0144dae9c88888876aa9b09
slevine3 <levinesam3@gmail.com>
2022-05-12 21:53:55 +0300

    user page completed

--- Admin/src/pages/User.jsx
+++ Admin/src/pages/User.jsx
@@ -5,6 +5,7 @@ import CalendarToday from "@material-ui/icons/CalendarToday";
 import PhoneAndroid from "@material-ui/icons/PhoneAndroid";
 import MailOutline from "@material-ui/icons/MailOutline";
 import LocationSearching from "@material-ui/icons/LocationSearching";
+import Publish from "@material-ui/icons/Publish";
 
 const User = () => {
   return (
@@ -51,7 +52,68 @@ const User = () => {
             </div>
           </div>
         </div>
-        <div className="userUpdate"></div>
+        <div className="userUpdate">
+          <span className="userUpdateTitle">Edit</span>
+          <form className="userUpdateForm">
+            <div className="userUpdateLeft">
+              <div className="userUpdateItem">
+                <label>Username</label>
+                <input
+                  type="text"
+                  placeholder="anna beck99"
+                  className="userUpdateInput"
+                />
+              </div>
+              <div className="userUpdateItem">
+                <label>Full Name</label>
+                <input
+                  type="text"
+                  placeholder="Anna Beck"
+                  className="userUpdateInput"
+                />
+              </div>
+              <div className="userUpdateItem">
+                <label>Phone</label>
+                <input
+                  type="text"
+                  placeholder="+1 808-345-6467"
+                  className="userUpdateInput"
+                />
+              </div>
+              <div className="userUpdateItem">
+                <label>Email</label>
+                <input
+                  type="text"
+                  placeholder="anna@gmail.com"
+                  className="userUpdateInput"
+                />
+              </div>
+
+              <div className="userUpdateItem">
+                <label>Address</label>
+                <input
+                  type="text"
+                  placeholder="New York | USA"
+                  className="userUpdateInput"
+                />
+              </div>
+            </div>
+            <div className="userUpdateRight">
+              <div className="userUpdateUpload">
+                <img
+                  src="https://media.istockphoto.com/photos/indoor-photo-of-handsome-european-guy-pictured-isolated-on-grey-to-picture-id1034357476?k=20&m=1034357476&s=612x612&w=0&h=OpiBGaVDU02LI1WVpb02Wza6AAdTGopRpf0Kx6q8V-Q="
+                  alt=""
+                  className="userUpdateImg"
+                />
+                <label htmlFor="file">
+                  <Publish className="userUpdateIcon"/>
+                </label>
+                <input type="file" id="file" style={{ display: "none" }} />
+              </div>
+              <button className="userUpdateButton">Update</button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
