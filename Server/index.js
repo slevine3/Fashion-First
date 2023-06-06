7f24dd2dcadcfc5122452bcd180fda243e1d97d6
Sam Levine <sam.l@shoesonline.co.il>
2023-06-06 13:03:48 +0300

    update packages

--- Server/index.js
+++ Server/index.js
@@ -24,7 +24,6 @@ app.use("/api/checkout", stripeRoute);
 
 app.use(express.static(path.join(__dirname, "build")));
 
-
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
