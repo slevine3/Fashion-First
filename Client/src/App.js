80eeafae8b88cfd88bfad9a2be118aaccdf47cb5
Sam Levine <levinesam3@gmail.com>
2023-06-08 16:39:21 +0300

    push to heroku

--- Client/src/App.js
+++ Client/src/App.js
@@ -16,7 +16,7 @@ import {
 import Success from "./pages/Success";
 import { useSelector } from "react-redux";
 import ScrollToTop from "./ScrollToTop";
-
+// Force push to master
 const App = () => {
   const user = useSelector((state) => state.user.currentUser);
 
